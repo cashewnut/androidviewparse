8bc80bded2a5c841b3c86ae49580cf2583f97b64
cashewnut <xiyaoguo@yeah.net>
2018-01-30 18:35:14 +0800

    xiyaoguo

--- src/main/java/edu/fdu/se/examples/english/Similar.java
+++ src/main/java/edu/fdu/se/examples/english/Similar.java
@@ -10,7 +10,7 @@ public class Similar {
 
     private String str1;
     private String str2;
-    private String dir = "/usr/loacl/WordNet/";
+    private String dir = "/usr/loacl/WordNet";
     private JWS    ws = new JWS(dir, "");
 
     public Similar(String str1,String str2){
